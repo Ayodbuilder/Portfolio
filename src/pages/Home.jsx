21e80ea3b20eac573d7f8712136a58633ffdda81
Ayodbuilder <olayinkavitor68@gmail.com>
2026-03-10 15:16:02 +0100

    fix case-sensitive import for Skills component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 
-import Skills from "../components/skills"
+import Skills from "../components/Skills"
 // eslint-disable-next-line no-unused-vars
 import { motion } from "framer-motion"
 import { Link } from "react-router-dom"
