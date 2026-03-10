63f77bf6ae4001ec6f0f9ca7210640b2384dd0b1
Ayodbuilder <olayinkavitor68@gmail.com>
2026-03-10 14:38:14 +0100

    fix about import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Home from "./pages/Home"
-import About from "./pages/about"
+import About from "./pages/About"
 import Projects from "./pages/Projects"
 import Contact from "./pages/Contact"
 import Writing from "./components/Writing"
